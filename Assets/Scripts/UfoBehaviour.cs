4372a411815abe7cba746b2f49ebf70e29c1782c
FémLol Stúdió <femlol1femlol@gmail.com>
2024-10-09 03:00:44 +0200

    qq

--- Assets/Scripts/UfoBehaviour.cs
+++ Assets/Scripts/UfoBehaviour.cs
@@ -6,22 +6,35 @@ public class UfoBehaviour : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        scale = Random.Range(40, 60);
+        r = Random.Range(-limits, limits);
     }
 
     // Update is called once per frame
+    const float limits = 15;
+    float scale = 20;
+
     void Update()
     {
+        r += Time.deltaTime * scale;
 
+        transform.eulerAngles = new(0, 0, r);
+
+        if (r > limits && scale > 0)
+            scale *= -1;
+        if (r < -limits && scale < 0)
+            scale *= -1;
     }
+
+    public float r = 0;
 
     const float targetTime = 1;
     const float randomRange = 1;
 
     public async void ChangePositionToPlanet(Vector3 target)
     {
-        target = new Vector3(target.x + Random.Range(-randomRange, randomRange), target.y + Random.Range(-randomRange, randomRange), target.z + Random.Range(-randomRange, randomRange));
         Vector3 oldPosition = transform.position;
+        target = new Vector3(target.x + Random.Range(-randomRange, randomRange), target.y + Random.Range(-randomRange, randomRange), oldPosition.z);
         float now = 0;
         while (now < targetTime)
         {
