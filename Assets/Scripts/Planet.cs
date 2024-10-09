21fc2f117cce6c2d172f020143798f530feb49b6
FémLol Stúdió <femlol1femlol@gmail.com>
2024-10-09 05:03:11 +0200

    pp

--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -1,6 +1,13 @@
+using TMPro;
 using UnityEngine;
 
 public class Planet : MonoBehaviour
 {
     public Vector3 coordinate;
+
+    public void Start()
+    {
+        TextMeshPro txt = transform.GetChild(1).GetComponent<TextMeshPro>();
+        txt.text = $"({coordinate.x:0.00}, {coordinate.y:0.00}, {coordinate.z:0.00})";
+    }
 }
